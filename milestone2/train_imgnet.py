0a94fd50da11b599416eafc2d2fc0de86330f817
tbonza <tylers.pile@gmail.com>
2019-03-19 17:30:27 +0000

    basic tensorflow model seems to be working, adding layers now

--- milestone2/train_imgnet.py
+++ milestone2/train_imgnet.py
@@ -17,39 +17,27 @@ import logging
 import os
 import time
 
+import tensorflow as tf
 from tensorflow.keras.datasets import cifar10
 
-from specify_imgnet_model import cifar_model
+from specify_imgnet_model import cifar_model, plot_costs, initialize_parameters
 from augment_data import SimulateCondition
 from logging_utils import enable_cloud_log
 
 
-enable_cloud_log()
+enable_cloud_log(level='DEBUG')
 logger = logging.getLogger(__name__)
 
 
 # Dataset of 50,000 32x32 color training images, 
 # labeled over 10 categories, and 10,000 test images.
 
-(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+(X_train, y_train), (X_test, y_test) = cifar10.load_data()
 
 # validate basic model 
 
 logger.info("Validating basic model")
-
-
-logger.info("Compiling basic model without metrics")
-#model.compile(loss='mean_absolute_error',
-#              optimizer='adam',
-#              metrics=['accuracy'])
-
-logger.info("Fitting basic model without metrics")
-#model.fit(x_train, x_train, epochs=1, batch_size=32)
-
-logger.info("Evaluating against basic test set without metrics")
-#loss_and_metrics = model.evaluate(x_test, x_test, batch_size=128)
-
-
+parameters, costs, lr = cifar_model(X_train, X_train, X_test, X_test)
 
 
 # now use a basic model with noise
