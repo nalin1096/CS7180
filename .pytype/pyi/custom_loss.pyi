967483b82e3cc75ccb3ad379576e126528e52777
tbonza <tylers.pile@gmail.com>
2019-04-06 20:25:13 -0400

    added pytype

---
+++ .pytype/pyi/custom_loss.pyi
@@ -0,0 +1,7 @@
+# (generated with --quick)
+
+from typing import Any
+
+K: module
+
+def mean_absolute_error(y_true, y_pred) -> Any: ...
---
+++ .pytype/pyi/model02.pyi
@@ -0,0 +1,14 @@
+# (generated with --quick)
+
+from typing import Any, Dict
+
+Conv2D: Any
+Dense: Any
+MaxPooling2D: Any
+Sequential: module
+
+class LeakyReLU(Any):
+    def __init__(self, **kwargs) -> None: ...
+
+
+def model02() -> Dict[str, Any]: ...
