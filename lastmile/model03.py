0571950927d668a3f179dbe60314c60d27143f7a
tbonza <tylers.pile@gmail.com>
2019-04-10 02:27:45 +0000

    added custom relu activation function

--- lastmile/model03.py
+++ lastmile/model03.py
@@ -4,6 +4,8 @@ from tensorflow.keras.activations import relu
 from tensorflow.keras.models import Sequential
 
 
+def my_relu(x):
+    return K.relu(x, alpha=0.2)
 
 def model03():
     """ Removed upsampling from model01. """
@@ -12,17 +14,17 @@ def model03():
     model = Sequential()
 
     # Block 1
-    model.add(Conv2D(8, (3,3), activation=K.relu,
+    model.add(Conv2D(8, (3,3), activation=my_relu,
                         padding='same', input_shape=(256,256,3)))
-    model.add(Conv2D(8, (3,3), padding='same', activation=K.relu))
+    model.add(Conv2D(8, (3,3), padding='same', activation=my_relu))
 
     # Block 2
-    model.add(Conv2D(16, (3,3), padding='same', activation=K.relu))
-    model.add(Conv2D(16, (3,3), padding='same', activation=K.relu))
+    model.add(Conv2D(16, (3,3), padding='same', activation=my_relu))
+    model.add(Conv2D(16, (3,3), padding='same', activation=my_relu))
 
     # Block 3
-    model.add(Conv2D(32, (3,3), padding='same', activation=K.relu))
-    model.add(Conv2D(32, (3,3), padding='same', activation=K.relu))
+    model.add(Conv2D(32, (3,3), padding='same', activation=my_relu))
+    model.add(Conv2D(32, (3,3), padding='same', activation=my_relu))
 
     # Block 4
     model.add(Dense(16))
