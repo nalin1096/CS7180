d5f2d52fd4e5a3240b77a17c6c506164e8fc2ed5
nalin1096 <nalin1096@hotmail.com>
2019-04-09 22:38:10 -0400

    using relu for activation

--- lastmile/model03.py
+++ lastmile/model03.py
@@ -3,10 +3,6 @@ from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dense
 from tensorflow.keras.activations import relu
 from tensorflow.keras.models import Sequential
 
-
-def my_relu(x):
-    return K.relu(x, alpha=0.2)
-
 def model03():
     """ Removed upsampling from model01. """
     model_id = 'model03'
@@ -16,15 +12,15 @@ def model03():
     # Block 1
     model.add(Conv2D(8, (3,3), activation=my_relu,
                         padding='same', input_shape=(256,256,3)))
-    model.add(Conv2D(8, (3,3), padding='same', activation=my_relu))
+    model.add(Conv2D(8, (3,3), padding='same', activation=K.relu))
 
     # Block 2
-    model.add(Conv2D(16, (3,3), padding='same', activation=my_relu))
-    model.add(Conv2D(16, (3,3), padding='same', activation=my_relu))
+    model.add(Conv2D(16, (3,3), padding='same', activation=K.relu))
+    model.add(Conv2D(16, (3,3), padding='same', activation=K.relu))
 
     # Block 3
-    model.add(Conv2D(32, (3,3), padding='same', activation=my_relu))
-    model.add(Conv2D(32, (3,3), padding='same', activation=my_relu))
+    model.add(Conv2D(32, (3,3), padding='same', activation=K.relu))
+    model.add(Conv2D(32, (3,3), padding='same', activation=K.relu))
 
     # Block 4
     model.add(Dense(16))
