eb65b72fbfcc252ef53ed2cea42e1cf4f612ecac
tbonza <tylers.pile@gmail.com>
2019-04-16 17:25:58 -0400

    added evaluate_model func, added nalin change to restore_model

--- lastmile/test_model.py
+++ lastmile/test_model.py
@@ -2,6 +2,7 @@
 
 https://www.tensorflow.org/tutorials/keras/save_and_restore_models
 """
+from datetime import datetime
 import logging
 import os
 
@@ -15,7 +16,7 @@ def restore_model(model_func, model_type):
     if os.path.isdir(save_dir):
         latest = tf.train.latest_checkpoint(save_dir)
 
-        model = model_func()
+        model = model_func.get('model', None)
         model.load_weights(latest)
         print(model.summary())
         return model
@@ -24,6 +25,21 @@ def restore_model(model_func, model_type):
         logger.error("savedir not found: {}".format(save_dir))
         return None
 
+def evaluate_model(test_dataflow, model, model_name):
+    """ Evalute trained model against test set. """
+    datetime_now = datetime.now().strftime("%Y%m%d-%H%M%S")
+    review_dir = os.path.join(os.getcwd(), 'review')
+    if not os.path.isdir(review_dir):
+        os.makedirs(review_dir)
+
+    logger.info("STARTED evaluating model: {}".format(model_name))
+    evaluate = model.evaluate_generator(test_dataflow)
+    test_eval = str(model.metrics_names) + str(evaluate)
+    test_eval_name = 'test_eval_{}_{}.txt'.format(model_name, datetime_now)
+    te_filepath = os.path.join(review_dir, test_eval_name)
+    with open(te_filepath, "w") as outfile:
+        outfile.write(test_eval)
+    
 def review_model():
     """ Predict an image, then stitch it together. """
     pass
