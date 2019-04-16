7d04dce2b14acefaadf76c7744a5bf7b178de512
tbonza <tylers.pile@gmail.com>
2019-04-16 17:14:27 -0400

    less sloppy restore function

--- lastmile/test_model.py
+++ lastmile/test_model.py
@@ -2,20 +2,31 @@
 
 https://www.tensorflow.org/tutorials/keras/save_and_restore_models
 """
+import logging
 import os
 
 import tensorflow as tf
 
-def restore_model(model_func):
+
+logger = logging.getLogger(__name__)
+
+def restore_model(model_func, model_type):
     save_dir = os.path.join(os.getcwd(), 'saved_models', model_type)
-    if not os.path.isdir(save_dir):
-        os.makedirs(save_dir)
-    latest = tf.train.latest_checkpoint(save_dir)
+    if os.path.isdir(save_dir):
+        latest = tf.train.latest_checkpoint(save_dir)
 
-    model = model_func()
-    model.load_weights(latest)
+        model = model_func()
+        model.load_weights(latest)
+        print(model.summary())
+        return model
 
-    return model
+    else:
+        logger.error("savedir not found: {}".format(save_dir))
+        return None
+
+def review_model():
+    """ Predict an image, then stitch it together. """
+    pass
     
 
 
